c8d5c31bf3423aa8dbd2c44f698fd9e12e38a020
Mateusz Pawlak <mpawlak62@gmail.com>
2016-05-25 09:54:20 +0200

    max likes per hour

--- bot.py
+++ bot.py
@@ -4,20 +4,12 @@ import json
 import time
 import sys
 from random import randint
+from operation import Operations
 
 class InstaLike:
-	login_url = 'https://www.instagram.com/accounts/login/?force_classic_login&hl=pl'
-	tag_url = 'https://www.instagram.com/explore/tags/{0}/'
-	like_url = 'https://www.instagram.com/web/likes/{0}/like/'
-	base_url = 'https://www.instagram.com/'
-
-	user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-	accept_language = 'pl-PL,pl;q=0.8,en-US;q=0.6,en;q=0.4'
-	content_type = 'application/x-www-form-urlencoded'
+	operation = Operations()
 
 	instagrams = ''
-	headers = ''
-	cookies = ''
 
 	response_fail = 0
 	total_failed_likes = 0
@@ -26,9 +18,14 @@ class InstaLike:
 	# instance stats
 	t0 = 0
 	t1 = 0
+
+	period_start = 0
+	period_time = 60 * 60
+
 	total_likes = 0
-	# after # of likes rest for 5h or whatever
 	current_likes = 0
+	max_likes_per_hour = 245
+	hourly_likes = 0
 
 	def __init__(self, login, password):
 		self.session = requests.Session()
@@ -36,72 +33,24 @@ class InstaLike:
 		self.password = password
 
 	def log_in(self):
-		self.payload = {
-			'username' : self.login,
-			'password' : self.password,
-			'csrfmiddlewaretoken' : ''
-		}
+		self.log_event('trying to log in')
 
-		# get csrftoken
-		response = self.session.get(self.base_url)
-		self.csrftoken = response.cookies['csrftoken']
-		self.payload['csrfmiddlewaretoken'] = self.csrftoken
+		response = self.operation.log_in(self.login, self.password)
 
-		# setup headers
-		self.headers = {
-			'origin' : self.base_url,
-			'referer' : self.login_url,
-			'User-Agent' : self.user_agent,
-			'upgrade-insecure-requests' : 1,
-			'content-length' : 97,
-			'content-type' : self.content_type,
-			'cache-control' : 'max-age=0',
-			'accept-language' : self.accept_language,
-			'accept-encoding' : 'gzip, deflate',
-			'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-		}
-
-		self.log_event('Trying to log in with credentials: username = "{0}" and password = "{1}"'.format(self.payload['username'], self.payload['password']))
-
-		# try to log in
-		response = self.session.post(self.login_url, data = self.payload, headers = self.headers, allow_redirects = True)
-		if (response.status_code != 200):
+		if (not response):
 			self.log_event('Could not log in, status code: {0}'.format(response.status_code))
 			return False
 		else:
-			self.headers['referer'] = response.url
 			self.t0 = time.time()
+			self.period_start = time.time()
 			self.log_event('Logged in successfully.')
-			# was redirected
-			if (len(response.history) > 0):
-				self.log_event('\tHistory: {0}'.format(response.history))
 			return True
 
 	def get_instagrams(self):
 		self.log_event('getting posts...')
-		response = self.session.get(self.tag_url.format('l4l'), headers = self.headers)
-		if (response.status_code != 200):
-			self.log_event('Could not get instagrams. Status code: {0}'.format(response.status_code))
-			return False
+		self.instagrams = self.operation.get_photos_by_tag('l4l')
 
-		self.headers = {
-			'accept' : '*/*',
-			'accept-encoding' : 'gzip, deflate',
-			'accept-language' : self.accept_language,
-			'content-length' : 0,
-			'origin' : self.base_url,
-			'referer' : response.url,
-			'user-agent' : self.user_agent,
-			'x-csrftoken' : response.cookies['csrftoken'],
-			'x-instagram-ajax' : 1,
-			'x-request-with' : 'XMLHttpRequest'
-		}
-
-		self.cookies = response.cookies
-
-		posts = re.search(b'_sharedData = ({.+?});</script>', response.content)
-		if(posts):
-			self.instagrams = json.loads(posts.group(1).decode('utf-8'))['entry_data']['TagPage'][0]['tag']['media']['nodes']
+		if (self.instagrams):
 			self.log_event('found {0} posts matching criteria out of {1}'.format(len(list(filter(lambda x : x['likes']['count'] < 10, self.instagrams))), len(self.instagrams)))
 			time.sleep(randint(5,7))
 			return True
@@ -113,12 +62,16 @@ class InstaLike:
 		to_like = len(list(filter(lambda x : x['likes']['count'] < 10, self.instagrams)))
 		liked = 0
 		self.current_likes = 0
+		self.response_fail = 0 # count fails that happen to occur in short amount of time
 		last_error = 'none'
 		for instagram in self.instagrams:
-			if(instagram['likes']['count'] < 10):
-				response = self.session.post(self.like_url.format(instagram['id']), headers = self.headers, cookies = self.cookies, allow_redirects = True)
+			if(instagram['likes']['count'] < 100):
+				# todo
+				# self.log_event(instagram['likes'].get('viewer_has_liked', 'false'))
+				response = self.operation.like(instagram['id'])
 				# status code == 400 -> youre fucked! BAN.
 				if(response.status_code != 200):
+					self.hourly_likes += 1
 					self.current_likes += 1
 					last_error = response.status_code
 					if(response.status_code == 400):
@@ -131,6 +84,7 @@ class InstaLike:
 				self.t1 = time.time()
 				liked += 1
 				self.total_likes += 1
+				self.hourly_likes += 1
 				self.current_likes += 1
 				self.log_event('{0}/{1}\r'.format(self.current_likes, to_like), 0)
 				time.sleep(randint(1,3))
@@ -157,13 +111,23 @@ class InstaLike:
 			if(self.ban400 > 3):
 				self.log_event('You are banned?')
 			if(self.response_fail > 4):
-				self.log_event('Failed to like photo more than 4 times. Waiting for some time.')
+				self.log_event('Failed to like photo more than 4 times in short amount of time. Waiting for some time.')
 				time.sleep(10*randint(6,9))
 				self.response_fail = 0
 				self.log_in()
 			if(self.get_instagrams()):
 				self.like()
 				self.get_stats()
+			if (self.hourly_likes > self.max_likes_per_hour):
+				if (time.time() - self.period_start < self.period_time):
+					self.log_event('sleeping for {0} secs'.format(self.period_time - (time.time() - self.period_start)))
+					time.sleep(self.period_time - (time.time() - self.period_start))
+					self.period_start = time.time()
+					self.hourly_likes = 0
+				else:
+					# done under max in about hour
+					self.log_event('done under max in period')
+					self.hourly_likes = 0
 
 
 	def log_event(self, text, nl = 1):
