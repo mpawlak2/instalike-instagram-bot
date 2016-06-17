0399a5df88cc01984a2c9570bb8576f2ad2811ce
Mateusz Pawlak <mpawlak62@gmail.com>
2016-06-17 13:18:11 +0200

    fixed bug

--- content.py
+++ content.py
@@ -11,7 +11,8 @@ class ContentManager:
 		self.configuration = configuration
 		self.spam_validator = spam.SpamDetector(self.operation, self.repository)
 
-		self.tags = map(lambda tag: tag.strip(), self.configuration.instalike_tags.split(','))
+		self.tags = list(map(lambda tag: tag.strip(), self.configuration.instalike_tags.split(',')))
+		print(len(self.tags))
 
 		self.photos = []
 		self.photos_from_model = []
