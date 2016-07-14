fd8703500bb0aaa348fe18023bbcb148da6d1149
Mateusz Pawlak <mpawlak62@gmail.com>
2016-07-14 11:56:32 +0200

    minor update

--- configuration.py
+++ configuration.py
@@ -60,7 +60,6 @@ class Configuration:
 		self.instafollow_max_follows_per_hour = int(instafollow.get('maxfollowsperhour', 8))
 		self.instafollow_max_unfollows_per_hour = int(instafollow.get('maxunfollowsperhour', 2))
 		self.instafollow_unfollow_users = instafollow.getboolean('UnfollowUsers', False)
-		self.instafollow_unfollow_who_does_not_follow_back = instafollow.getboolean('unfollowwhennotfollowingback', True)
 
 		# BLACKLIST
 		self.banned_tags = blacklist.get('PhotoTagsList', None)
