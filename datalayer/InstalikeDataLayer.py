f9956f7907854d63c7a1224af5be165e91561682
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-04 18:51:25 +0100

    create abstract methods

--- datalayer/InstalikeDataLayer.py
+++ datalayer/InstalikeDataLayer.py
@@ -2,8 +2,34 @@ from abc import ABC, abstractmethod
 
 import model
 
-
+""" If you wish to have new persistence plugin - implement these methods. """
 class InstalikeDataLayer(ABC):
     @abstractmethod
     def persist_user(self, user: model.User):
         pass
+
+    @abstractmethod
+    def persist_photo(self, photo: model.Photo):
+        pass
+
+    @abstractmethod
+    def persist_like(self, photo: model.Photo):
+        pass
+
+    @abstractmethod
+    def persist_follow(self, user: model.User):
+        pass
+
+    @abstractmethod
+    def persist_unfollow(self, user: model.User):
+        pass
+
+    @abstractmethod
+    def persist_activity(self, activity: model.Activity):
+        pass
+
+    @abstractmethod
+    def get_users_to_unfollow(self, day_range):
+        pass
+
+
