dd347fb9a39d4ccd0eb174e9b20b7a71d0dd8f18
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-06 20:38:12 +0100

    minor update

--- logger/clogger.py
+++ logger/clogger.py
@@ -5,5 +5,5 @@ from logger.logger import Logger
 
 class CLogger(Logger):
     def log(self, message):
-        now = datetime.datetime.now()
+        now = datetime.datetime.today()
         print('[{0}]: {1}'.format(now, message))
