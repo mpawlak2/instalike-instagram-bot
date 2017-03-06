4bed3904128b2c47d6eb85792233e418d2db70c2
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-06 22:06:09 +0100

    update instalikedao tests

--- tests/dbplugin/test_instalikedao.py
+++ tests/dbplugin/test_instalikedao.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
 
 from dbplugin.instalikedao import InstalikeDAO
+from dbplugin.pgdatasource import PGDataSource
 
 
 class TestInstalikeDAO(TestCase):
+    def makeDataSource(self):
+        user = 'postgres'
+        password = 'postgres'
+        host = 'localhost'
+        dbname = 'instalike'
+
+        dataSource = PGDataSource(user, password, host, dbname)
+        return dataSource
+
     def test_classInstalikeDAOImplementsAllMethods(self):
         try:
-            object = InstalikeDAO()
+            object = InstalikeDAO(self.makeDataSource())
         except TypeError:
             self.fail('Error instantiating InstalikeDAO class.')
 
+    def test_shouldNotCreateInstalikeDAOWithoutDatasource(self):
+        datasource = self.makeDataSource()
+        dao = InstalikeDAO(datasource)
+        self.assertIsNotNone(dao)
