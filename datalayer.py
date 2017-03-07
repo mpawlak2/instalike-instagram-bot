7e1334cae078dcd51dce472b030d0ef00d85fc14
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-07 23:41:55 +0100

    cleaning up, moving classes etc.

--- datalayer.py
+++ datalayer.py
@@ -1,12 +1,9 @@
-import postgresql
+from peewee import *
 import model
 from abc import ABC, abstractmethod
-from peewee import SqliteDatabase
-from logger.logger import Logger
 
 # SQLite database connection
 sqlite_db = SqliteDatabase('instalike.db')
-
 
 """ If you wish to have new persistence plugin derive from this class and implement these methods. """
 
@@ -111,3 +108,42 @@ class InstalikeSQLDAO(InstalikeDataLayer):
         sql_query = 'select unfollow(_user_id := {0}, _status_code := 200)'.format(user.id)
 
         self.data_source.get_connection().execute(sql_query)
+
+
+# Model Definitions
+
+class BaseModel(Model):
+    class Meta:
+        database = sqlite_db
+
+
+class Photo(BaseModel):
+    width = IntegerField()
+    height = IntegerField()
+    code = CharField()
+    is_ad = BooleanField()
+    likes_count = IntegerField()
+    viewer_has_liked = BooleanField()
+    is_video = BooleanField()
+    display_src = CharField()
+    location = CharField()
+    owner_id = IntegerField()
+    caption = CharField()
+    owner_name = CharField()
+
+
+class User(BaseModel):
+    name = CharField()
+    has_blocked_viewer = BooleanField()
+    follows_count = IntegerField()
+    followers_count = IntegerField()
+    external_url = CharField()
+    follows_viewer = BooleanField()
+    profile_pic_url = CharField()
+    is_private = BooleanField()
+    full_name = CharField()
+    posts_count = IntegerField()
+    blocked_by_viewer = BooleanField()
+    followed_by_viewer = BooleanField()
+    is_verified = BooleanField()
+    biography = CharField()
