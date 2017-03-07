c5ceb6414e57b29076e7317993f6acf587e69dd1
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-07 23:33:59 +0100

    create user model

--- models.py
+++ models.py
@@ -5,6 +5,9 @@ class BaseModel(Model):
     class Meta:
         database = SqliteDatabase('instalike.db')
 
+        def __init__(self):
+            self.database.connect()
+
 
 class Photo(BaseModel):
     width = IntegerField()
@@ -19,3 +22,19 @@ class Photo(BaseModel):
     owner_id = IntegerField()
     caption = CharField()
     owner_name = CharField()
+
+class User(BaseModel):
+    name = CharField()
+    has_blocked_viewer = BooleanField()
+    follows_count = IntegerField()
+    followers_count = IntegerField()
+    external_url = CharField()
+    follows_viewer = BooleanField()
+    profile_pic_url = CharField()
+    is_private = BooleanField()
+    full_name = CharField()
+    posts_count = IntegerField()
+    blocked_by_viewer = BooleanField()
+    followed_by_viewer = BooleanField()
+    is_verified = BooleanField()
+    biography = CharField()
