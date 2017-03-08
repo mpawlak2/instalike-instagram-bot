bd5dc50c2fb132ef319e0a2c900141f2be694868
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-08 20:51:43 +0100

    test should persist like - fail

--- tests/test_instalikedatalayer.py
+++ tests/test_instalikedatalayer.py
@@ -48,3 +48,23 @@ class TestInstalikeDataLayer(TestCase):
 
         self.assertEqual(rows, 1)
 
+    def test_should_persist_like(self):
+        test_photo = Photo().from_json(json.loads('''
+                            {"code": "tedwefeqwfa4s",
+                            "dimensions": {"width": 1080, "height": 1349},
+                            "comments_disabled": false,
+                            "owner": {"id": "11111111"},
+                            "comments": {"count": 134},
+                            "caption": "#sunday#black#girl#selfie",
+                            "likes": {"count": 12},
+                            "date": 1476637330,
+                            "thumbnail_src": "https://scontent-waw1-1.cdninstagram.com/whatever/whatever",
+                            "is_video": false,
+                            "id": "3333333333333333333",
+                            "display_src": "https://scontent-waw1-1.cdninstagram.com/whatever/whatever"}'''))
+
+        dao = InstalikeSQLDAO()
+        rows = dao.persist_like(test_photo)
+
+        self.assertEqual(rows, 1)
+
