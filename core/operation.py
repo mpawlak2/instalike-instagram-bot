1d09413e43309b18baafe7f9c28e8b8f5108d5ae
Mateusz Pawlak <mpawlak62@gmail.com>
2017-03-15 23:24:49 +0100

    create method signatures in new operations class

--- core/operation.py
+++ core/operation.py
@@ -1,2 +1,24 @@
 class Operations:
-    pass
+    def log_id(self, account):
+        pass
+
+    def log_out(self, account):
+        pass
+
+    def like_media(self, photo_id):
+        pass
+
+    def unlike_media(self, photo_id):
+        pass
+
+    def follow_user(self, user_id):
+        pass
+
+    def unfollow_user(self, user_id):
+        pass
+
+    def comment_media(self, media_id):
+        pass
+
+    def block_user(self, user_id):
+        pass
