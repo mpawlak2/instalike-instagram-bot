663b8a04a6266e4a3e43b46290178c643eee1043
Mateusz Pawlak <mpawlak62@gmail.com>
2017-03-23 20:37:59 +0100

    common operations in base class

--- core/algorithms.py
+++ core/algorithms.py
@@ -2,14 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class Algorithm(ABC):
+    __operations = None
+
+    def __init__(self, operations):
+        self.__operations = operations
+
     @abstractmethod
     def run(self):
         pass
 
 
 class LikeAlgorithm(Algorithm):
-    __operations = None
-
     def __init__(self, operations):
         self.__operations = operations
 
