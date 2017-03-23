1732bdc3273243f4c3d285f05f7a9c3f52b5bf73
Pawlak Mateusz <mpawlak62@gmail.com>
2017-03-23 15:29:36 +0100

    create followalgorithm class

--- core/algorithms.py
+++ core/algorithms.py
@@ -15,3 +15,11 @@ class LikeAlgorithm(Algorithm):
 
     def run(self):
         pass
+
+
+class FollowAlgorithm(Algorithm):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
