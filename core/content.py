9a773f1a17b7802bbe6e96e57c86a0ddc4d06e7b
Mateusz Pawlak <mpawlak62@gmail.com>
2017-03-26 19:52:06 +0200

    add get next media method

--- core/content.py
+++ core/content.py
@@ -9,17 +9,27 @@ class MediaAlgorithm(ABC):
 
 class TagMediaAlgorithm(MediaAlgorithm):
     __tag_list = []
+    __operations = None
 
-    def __init__(self, tag_list):
+    def __init__(self, tag_list, operations):
         self.__tag_list = tag_list
+        self.__operations = operations
 
     def get_media(self):
-        pass
+        media_list = []
+        tag = self.__tag_list.pop()
 
 
 class ContentManager:
     __media_list = []
     __media_algorithm = None
+    __media_generator = None
+
+    def get_next_media(self):
+        if self.__media_generator is None:
+            self.__media_generator = self.get_media_generator()
+
+        return next(self.__media_generator)
 
     def get_media_generator(self):
         if not self.__media_list:
